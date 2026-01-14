7ca63fc5764dac4fd37dae795657d9f89dffe526
Francisco Blanco <blancofrancisco34@gmail.com>
2026-01-14 16:11:50 -0500

    add update

---
+++ Readme.md
@@ -0,0 +1,48 @@
+# Aveonine Modulo de Campaña Slug
+
+Contributors: Francisco Blanco
+
+Tags: aveonline, elementor, url slug
+
+Requires at least: 5.0
+
+Tested up to: 5.9
+
+Stable tag: 1.0.0
+
+License: GPLv2 or later
+
+License URI: [https://www.gnu.org/licenses/gpl-2.0.html](https://www.gnu.org/licenses/gpl-2.0.html)
+
+It is an plugin of wordpress, for Shipping in Colombia
+
+## Description
+
+Agrega un modulo para agregar la url de campaña por pagina.
+
+### Installation
+
+1. Download .zip [__CLICK HERE__](https://github.com/franciscoblancojn/aveonline-module-campana-slug/archive/refs/heads/master.zip) in repository [https://github.com/franciscoblancojn/aveonline-module-campana-slug](https://github.com/franciscoblancojn/aveonline-module-campana-slug)
+2. Go to admin of site wordpress, in __Plugins -> Add New__
+3. Click in button __Upload Plugin__
+4. Click in buttom __Select File__, and Upload file
+5. Active Plugin
+
+## Developer
+
+* Name: Francisco Blanco
+* Website: https://franciscoblanco.vercel.app/
+* Email: blancofrancisco34@gmail.com
+
+## Repositories
+
+* GitHub: https://github.com/franciscoblancojn/aveonline-module-campana-slug
+
+## License
+This plugin is distributed under the terms of the GNU General Public License v2.0 or later.
+
+
+
+
+
+
--- aveonline-module-campana-slug.php
+++ aveonline-module-campana-slug.php
@@ -11,6 +11,66 @@
 
 if (!defined('ABSPATH')) exit;
 
+
+//AVMCUS_
+define("AVMCUS_KEY",'AVMCUS');
+define("AVMCUS_LOG",true);
+define("AVMCUS_LOG_COUNT",1000);
+define("AVMCUS_BASENAME",plugin_basename(__FILE__));
+define("AVMCUS_DIR",plugin_dir_path( __FILE__ ));
+define("AVMCUS_URL",plugin_dir_url(__FILE__));
+
+require_once AVMCUS_DIR . 'update.php';
+github_updater_plugin_wordpress([
+    'basename'=>AVMCUS_BASENAME,
+    'dir'=>AVMCUS_DIR,
+    'file'=>"index.php",
+    'path_repository'=>'franciscoblancojn/aveonline-module-campana-slug',
+    'branch'=>'master',
+    'token_array_split'=>[
+        "g",
+        "h",
+        "p",
+        "_",
+        "G",
+        "4",
+        "W",
+        "E",
+        "W",
+        "F",
+        "p",
+        "V",
+        "U",
+        "E",
+        "F",
+        "V",
+        "x",
+        "F",
+        "U",
+        "n",
+        "b",
+        "M",
+        "k",
+        "P",
+        "R",
+        "x",
+        "o",
+        "f",
+        "t",
+        "Y",
+        "8",
+        "z",
+        "j",
+        "t",
+        "4",
+        "E",
+        "x",
+        "b",
+        "i",
+        "9"
+    ]
+]);
+
 // Verificar Elementor
 add_action('plugins_loaded', function () {
     if (!did_action('elementor/loaded')) {
